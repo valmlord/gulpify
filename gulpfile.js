a5b17ea64d10c5114a79f08b497507dcef9ab7c3
Stepan Korshukov <korshukovstepan@gmail.com>
2024-08-17 20:43:16 +0300

    feat: add browsersync to gulp boileerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-import { src, dest, series, watch } from 'gulp';
+import { src, dest, series, watch, parallel } from 'gulp';
 import fileInclude from 'gulp-file-include';
 import htmlMin from 'gulp-htmlmin';
 import size from 'gulp-size';
+import browserSync from 'browser-sync';
+
+const server = browserSync.create();
 
 const buildHTML = () =>
   src('app/html/*.html')
@@ -14,12 +17,22 @@ const buildHTML = () =>
       }),
     )
     .pipe(size({ title: 'HTML after compression:' }))
-    .pipe(dest('./build'));
+    .pipe(dest('./build'))
+    .pipe(server.stream());
+
+const serve = () => {
+  server.init({
+    server: {
+      baseDir: './build',
+    },
+  });
+};
 
 const watcher = () => {
   watch('app/html/**/*.html', buildHTML);
+  watch('build/*.html').on('change', server.reload);
 };
 
-export { buildHTML, watcher };
+export { buildHTML, watcher, serve };
 
-export default series(buildHTML, watcher);
+export default series(buildHTML, parallel(serve, watcher));
